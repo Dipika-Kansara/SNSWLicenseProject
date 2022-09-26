e942ad751cf5cffaa2778b5d51ccf1e61afcabcb
Jijantd <jijan.tuladhar2@studytafensw.edu.au>
2022-09-26 16:28:02 +1000

    login update

--- src/main/kotlin/Routes/AccountRoute.kt
+++ src/main/kotlin/Routes/AccountRoute.kt
@@ -1,4 +1,6 @@
 import Models.User
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.mongodb.client.MongoDatabase
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -7,6 +9,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.litote.kmongo.*
 import org.mindrot.jbcrypt.BCrypt
+import java.util.*
 
 
 fun Route.accountRoute (db:MongoDatabase) {
@@ -17,16 +20,39 @@ fun Route.accountRoute (db:MongoDatabase) {
 
         post("/register") {
             val data = call.receive<User>()
-            val hashed = BCrypt.hashpw(data.password,BCrypt.gensalt())
+            val hashed = BCrypt.hashpw(data.password, BCrypt.gensalt())
             val user = User(data.username, password = hashed, roles = listOf("customers"))
             usersCollection.insertOne(user)
             call.respond(HttpStatusCode.Created)
 
         }
 
+        post("/login") {
+            val data = call.receive<User>()
+
+            val filter = "{username:/^${data.username}$/i}"
+            val user = usersCollection.findOne(filter)
+
+            if (user == null) {
+                return@post call.respond(HttpStatusCode.BadRequest)
+            }
+            val valid = BCrypt.checkpw(data.password, user.password)
+            if (!valid) {
+                return@post call.respond(HttpStatusCode.BadRequest)
+            }
+            val expiry = Date(System.currentTimeMillis() + 86400000)
+            val token = JWT.create()
+                .withAudience("http://localhost:8080")
+                .withIssuer("http://localhost:8080")
+                .withClaim("username", user?.username)
+                .withClaim("roles", user?.roles)
+                .withExpiresAt(expiry)
+                .sign(Algorithm.HMAC256("secret"))
+
+            return@post call.respond(token)
 
 
+        }
 
     }
-
 }
